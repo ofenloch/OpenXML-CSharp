8513d1b4e993fd9f085abc6de817d9a0558864ba
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-08-24 20:45:17 +0200

    initial commit after 'dotnet new console --name OpenXML'

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
